225f3eba80d243a2a3067ac522e3342146d29711
flownxd <dima6pro@gmail.com>
2026-07-04 14:23:17 +0600

    Add task07: Class attributes and reflection

---
+++ task07/SampleClass.cs
@@ -0,0 +1,20 @@
+namespace task07;
+
+[DisplayName("Пример класса")]
+[Version(1, 0)]
+public class SampleClass
+{
+    [DisplayName("Числовое свойство")]
+    public int Number { get; set; }
+
+    [DisplayName("Тестовый метод")]
+    public void TestMethod()
+    {
+    }
+
+    public void AnotherMethod()
+    {
+    }
+
+    public string Name { get; set; }
+}
